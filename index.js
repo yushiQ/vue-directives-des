59f5f0aa6a694bac4903e2a8144a4987e4b86df0
stonehe <stonehe@lexinfintech.com>
2019-09-23 17:48:40 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,11 @@
+import copy from './lib/copy.js';
+// 自定义指令
+const directives = { copy };
+// 这种写法可以批量注册指令
+export default {
+    install(Vue) {
+        Object.keys(directives).forEach((key) => {
+            Vue.directive(key, directives[key]);
+        });
+    },
+};
